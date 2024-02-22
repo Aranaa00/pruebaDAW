e0761583f0c8d95fe9250628ca9724c41a680de9
Aranaa00 <manuel.arana.saborido.alu@iesfernandoaguilar.es>
2024-02-22 17:06:39 +0100

    commit del .gitignore

--- usando_git.java
+++ usando_git.java
@@ -1,6 +1,7 @@
 public class usando_git{
 	public static void main(String[] args){
 		System.out.println("Proyecto con GIT");
-		System.out.p`rintln("Modificacion a la clase");
+		System.out.println("Modificacion a la clase");
+		System.out.println("gags");
 	}
 }
